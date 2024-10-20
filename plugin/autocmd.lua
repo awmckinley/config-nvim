5a818271bb419ba01536f86c24727dc383e59379
Adam McKinley <adam@awmckinley.net>
2024-10-19 21:31:03 -0500

    feat: set execute bit if shebang present

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -13,3 +13,24 @@ custom.autocmd("BufWritePre", {
 	end,
 })
 
+if jit.os == "Linux" or jit.os == "OSX" then
+	custom.autocmd("BufWritePost", {
+		desc = "Make scripts executable",
+		callback = function()
+			local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
+			if first_line and first_line:match("^#!") then
+				local out = vim.fn.system({
+					"chmod",
+					"a+x",
+					vim.fn.expand("%"),
+				})
+				if vim.v.shell_error ~= 0 then
+					vim.api.nvim_echo({
+						{ "Failed to set execute bit:\n", "ErrorMsg" },
+						{ out, "WarningMsg" },
+					}, true, {})
+				end
+			end
+		end,
+	})
+end
