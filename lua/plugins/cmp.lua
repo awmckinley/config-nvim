4a5ca265996333c5663f43a80b85be78197eb334
Adam McKinley <adam@awmckinley.net>
2024-10-28 22:55:34 -0500

    feat: tweak LSP completion for markdown-oxide

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -54,11 +54,28 @@ return {
 					end,
 				},
 				sources = {
-					{ name = "lazydev", group_index = 1 },
-					{ name = "codeium", group_index = 2 },
-					{ name = "luasnip" },
-					{ name = "nvim_lsp" },
-					{ name = "buffer" },
+					{
+						name = "lazydev",
+						group_index = 1,
+					},
+					{
+						name = "codeium",
+						group_index = 2,
+					},
+					{
+						name = "luasnip",
+					},
+					{
+						name = "nvim_lsp",
+						option = {
+							markdown_oxide = {
+								keyword_pattern = [[\(\k\| \|\/\|#\)\+]],
+							},
+						},
+					},
+					{
+						name = "buffer",
+					},
 				},
 			})
 
@@ -67,10 +84,14 @@ return {
 				mapping = {
 					["<C-N>"] = cmp.mapping.select_next_item(),
 					["<C-P>"] = cmp.mapping.select_prev_item(),
-					["<C-Y>"] = cmp.mapping.confirm({ select = true }),
+					["<C-Y>"] = cmp.mapping.confirm({
+						select = true,
+					}),
 				},
 				sources = {
-					{ name = "buffer" },
+					{
+						name = "buffer",
+					},
 				},
 			})
 
@@ -78,8 +99,12 @@ return {
 			cmp.setup.cmdline(":", {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = {
-					{ name = "path" },
-					{ name = "cmdline" },
+					{
+						name = "path",
+					},
+					{
+						name = "cmdline",
+					},
 				},
 				matching = {
 					disallow_fuzzy_matching = false,
@@ -94,16 +119,24 @@ return {
 			-- setup gitcommit
 			cmp.setup.filetype("gitcommit", {
 				sources = {
-					{ name = "git" },
-					{ name = "buffer" },
+					{
+						name = "git",
+					},
+					{
+						name = "buffer",
+					},
 				},
 			})
 
 			-- setup SQL
 			cmp.setup.filetype({ "mysql", "plsql", "sql" }, {
 				sources = {
-					{ name = "vim-dadbod-completion" },
-					{ name = "buffer" },
+					{
+						name = "vim-dadbod-completion",
+					},
+					{
+						name = "buffer",
+					},
 				},
 			})
 		end,
