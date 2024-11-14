7e3415928fed833261ff84580c892e1e4fbe67a6
Adam McKinley <adam@awmckinley.net>
2024-11-14 07:38:38 -0600

    feat: use superhtml binary on Darwin

--- nix/modules/html.nix
+++ nix/modules/html.nix
@@ -11,14 +11,24 @@
       # language server for emmet.io
       # replaces: emmet-ls
       emmet-language-server
-
+    ]
+    ++ lib.optionals pkgs.stdenv.isDarwin [
       # HTML language server and templating language library
-      superhtml
+      superhtml-bin
     ]
     ++ lib.optionals pkgs.stdenv.isLinux [
+      # HTML language server and templating language library
+      superhtml
+
       # HTML/CSS/JSON/ESLint language servers
       vscode-langservers-extracted
     ];
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      superhtml-bin = final.callPackage ../packages/superhtml-bin.nix { };
+    })
+  ];
 }
 // lib.optionalAttrs isDarwin {
   homebrew.brews = [
