76d545b62ed83863cc2914a8058e0ee52385086e
Adam McKinley <adam@awmckinley.net>
2024-12-26 10:09:04 -0600

    feat: add huggingface-hub

--- nix/modules/python.nix
+++ nix/modules/python.nix
@@ -5,10 +5,13 @@
     basedpyright
 
     # high-level dynamically-typed programming language
-    (python3.withPackages (
+    (python312.withPackages (
       ps: with ps; [
         # implementation of the Debug Adapter Protocol for Python
         debugpy
+
+        # download and publish models and other files on the huggingface.co hub
+        huggingface-hub
 
         # Python library to read/write Excel 2010 xlsx/xlsm files
         openpyxl
